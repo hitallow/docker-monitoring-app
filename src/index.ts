79d72820c23926b0aa685096c59c487111e392e3
Hítallo William <hitallo91@gmail.com>
2022-06-24 20:19:03 -0300

    feat: setup routes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
-import { collectInformations } from './main/scripts/collectContainers'
-import { logger } from './services/helpers/logger/logger'
+import express from 'express'
+import * as routes from '@src/main/routes/index'
+import { logger } from './services/helpers/logger'
 
-logger.info('Starting script...')
-collectInformations()
+const app = express()
+
+app.use(express.json())
+
+// setup routes config
+const routesDefinitions = Object.values(routes)
+routesDefinitions.forEach(route => app.use(route))
+
+app.listen(3000, () => {
+  logger.info('⚡ Server started on port 3000')
+})
