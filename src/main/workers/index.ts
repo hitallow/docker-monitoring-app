eea933551d3485b12c98820b9cb37648263f5ccd
Hítallo William <hitallo91@gmail.com>
2022-06-24 20:21:49 -0300

    feat: setup workers

--- src/main/workers/index.ts
+++ src/main/workers/index.ts
@@ -4,13 +4,17 @@ import { QueuesConfig } from '@src/infra/queues'
 import { processors } from '@src/main/processors'
 import { logger } from '@src/services/helpers/logger/logger'
 
-Object.entries(QueuesConfig).forEach(([queueName, queue]) => {
-  logger.info('Loading process to ', { queueName })
-  const processor = processors[queueName]
-  if (!processor) {
-    logger.error('No processor found for queue', { queueName })
-    return
-  }
-  queue.process(processor)
-  logger.info('Process loaded', { queueName })
-})
+function setupQueues() {
+  Object.entries(QueuesConfig).forEach(async ([queueName, queue]) => {
+    logger.info('Loading process to ', { queueName })
+    const processor = processors[queueName]
+    if (!processor) {
+      logger.error('No processor found for queue', { queueName })
+      return
+    }
+    queue.process(await processor())
+    logger.info('Process loaded', { queueName })
+  })
+}
+
+setupQueues()
