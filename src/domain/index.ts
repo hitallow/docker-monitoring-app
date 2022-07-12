dad467db8454bf639b31ff2963cc2975c18cef62
Hítallo William <hitallo91@gmail.com>
2022-07-12 02:09:24 -0300

    feat: add settings model

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,3 +1,4 @@
 export { ContainerConfig } from './models/container_config'
 export { ContainerStats } from './models/container_stats'
 export { Experiment } from './models/experiment'
+export { Settings } from './models/settings'
--- src/domain/models/index.ts
+++ src/domain/models/index.ts
@@ -1,3 +1,4 @@
 export { ContainerConfig } from './container_config'
 export { ContainerStats } from './container_stats'
 export { Experiment } from './experiment'
+export { Settings } from './settings'
